91dede3b37e878baa9afef9a312444c262a5e164
Tihor Nezic <tihor.nezic@gmail.com>
2021-03-02 16:37:16 +0100

    edit-put-route

--- routes/articles.js
+++ routes/articles.js
@@ -7,6 +7,11 @@ router.get('/new', (req, res) => {
     res.render('articles/new', {article: new Article()})
 })
 
+router.get('/edit/:id', async (req, res) => {
+    const article = await Article.findById(req.params.id)
+    res.render('articles/edit', {article: article})
+})
+
 router.get('/:slug', async (req, res) => {
     // findById is asynchronus function
     const article = await Article.findOne({slug: req.params.slug})
@@ -28,6 +33,20 @@ router.post('/', async (req, res) => {
     } catch (e) {
         console.log(e)
         res.render('articles/new', {article: article})
+    }
+})
+
+router.put('/:id', async (req, res) => {
+    let article = await Article.findById(req.params.id)
+    article.title = req.body.title
+    article.description = req.body.description
+    article.markdown = req.body.markdown
+    try {
+        article = await article.save()
+        res.redirect(`/articles/${article.slug}`)
+    } catch (e) {
+        console.log(e)
+        res.render('articles/edit', {article: article})
     }
 })
 
