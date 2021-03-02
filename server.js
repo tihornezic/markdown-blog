73d4c7ee04b0f2e5bf06fe9cfaf2338da9f9dfdf
Tihor Nezic <tihor.nezic@gmail.com>
2021-03-02 15:36:06 +0100

    show

--- server.js
+++ server.js
@@ -3,28 +3,26 @@
 
 const express = require('express')
 const app = express()
+const mongoose = require('mongoose')
 const articleRouter = require('./routes/articles')
+const Article = require('./models/article')
+
+mongoose.connect('mongodb://localhost/blog', { useNewUrlParser: true, useUnifiedTopology: true })
 
 // setting up ejs as view engine
 app.set('view engine', 'ejs')
 
-// every single route in articleRouter is going to be added at the end of /articles
-app.use('/articles', articleRouter)
+// to access all different parameters from article form inside of articles route - req.body ...
+// needs to come before using routers
+app.use(express.urlencoded({extended: false}))
 
 // index route
-app.get('/', (req, res) => {
-    const articles = [{
-        title: 'Test Article',
-        createdAt: new Date(),
-        description: 'Test description'
-    },
-    {
-        title: 'Test Article 2',
-        createdAt: new Date(),
-        description: 'Test description 2'
-    }
-]
+app.get('/', async (req, res) => {
+    const articles = await Article.find().sort({createdAt: 'desc'})
     res.render('articles/index', {articles: articles})
 })
+
+// every single route in articleRouter is going to be added at the end of /articles
+app.use('/articles', articleRouter)
 
 app.listen(5001, () => console.log('Server listening on port 5001.'))
