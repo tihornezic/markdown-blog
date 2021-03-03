619eb0e8ac564c3eb8994fbfca574741d368e861
Tihor Nezic <tihor.nezic@gmail.com>
2021-03-03 11:12:53 +0100

    refactored-post-put-routes-using-middleware-edit-button-on-index

--- routes/articles.js
+++ routes/articles.js
@@ -3,6 +3,10 @@ const express = require('express')
 const router = express.Router()
 const Article = require('../models/article')
 
+// since a link only does GET, and form POST/GET,
+// methodoverride was used to use/override DELETE method in form;
+// allows us to use DELETE/PATCH/PUT...
+
 router.get('/new', (req, res) => {
     res.render('articles/new', {article: new Article()})
 })
@@ -16,43 +20,43 @@ router.get('/:slug', async (req, res) => {
     // findById is asynchronus function
     const article = await Article.findOne({slug: req.params.slug})
     // if we cannot find an article, redirect user back to the home page
-    if( article == null) res.redirect('/')
+    if (article == null) res.redirect('/')
     res.render('articles/show', {article: article})
 })
 
-router.post('/', async (req, res) => {
-    let article = new Article({
-        title: req.body.title,
-        description: req.body.description,
-        markdown: req.body.markdown
-    })
-    try {
-        // asynchronus function
-        article = await article.save()
-        res.redirect(`/articles/${article.slug}`)
-    } catch (e) {
-        console.log(e)
-        res.render('articles/new', {article: article})
-    }
-})
+router.post('/', async (req, res, next) => {
+    req.article = new Article()
+    // go onto the next function in our list, which is saveArticleAndRedirect()
+    next()
+}, saveArticleAndRedirect('new'))
 
-router.put('/:id', async (req, res) => {
-    let article = await Article.findById(req.params.id)
-    article.title = req.body.title
-    article.description = req.body.description
-    article.markdown = req.body.markdown
-    try {
-        article = await article.save()
-        res.redirect(`/articles/${article.slug}`)
-    } catch (e) {
-        console.log(e)
-        res.render('articles/edit', {article: article})
-    }
-})
+router.put('/:id', async (req, res, next) => {
+    req.article = await Article.findById(req.params.id)
+    next()
+}, saveArticleAndRedirect('edit'))
 
 router.delete('/:id', async (req, res) => {
     await Article.findByIdAndDelete(req.params.id)
     res.redirect('/')
 })
+
+// returns a set of middleware
+function saveArticleAndRedirect(path) {
+    return async (req, res) => {
+        let article = req.article
+        article.title = req.body.title
+        article.description = req.body.description
+        article.markdown = req.body.markdown
+        try {
+            // asynchronus function
+            article = await article.save()
+            res.redirect(`/articles/${article.slug}`)
+        } catch (e) {
+            console.log(e)
+            // path will be either new or edit
+            res.render(`articles/${path}`, {article: article})
+        }
+    }
+}
 
 module.exports = router
